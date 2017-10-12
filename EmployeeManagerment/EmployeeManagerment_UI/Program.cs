5a22c7f9b2812c104a637a2b3886d5fc2be884e1
phuc <pnxphuc96@gmail.com>
2017-10-12 19:38:53 +0700

    Form salary + form educationlevel

--- EmployeeManagerment/EmployeeManagerment_UI/Program.cs
+++ EmployeeManagerment/EmployeeManagerment_UI/Program.cs
@@ -16,24 +16,19 @@ namespace EmployeeManagerment_UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
-            Application.Run(new User.MainForm());
+            //Application.Run(new User.MainForm());
             //Application.Run(new User.ContractForm());
             //Application.Run(new User.LoginForm());
             //Application.Run(new User.SuppliesForm());
             //Application.Run(new User.ContractForm());
             //Application.Run(new User.EventForm());
-=======
-
-            Application.Run(new User.ContractForm());
-
+            //Application.Run(new User.ContractForm());
             //Application.Run(new User.LoginForm());
             //Application.Run(new User.EmployeeForm());
-            Application.Run(new User.SuppliesForm());
+            //Application.Run(new User.SuppliesForm());
             //Application.Run(new User.ContractForm());
-            Application.Run(new User.EventForm());
+            Application.Run(new User.SalaryForm());
 
->>>>>>> Phuc
         }
     }
 }
