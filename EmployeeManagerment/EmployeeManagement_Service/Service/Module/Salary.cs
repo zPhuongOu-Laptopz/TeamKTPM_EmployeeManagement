f4ee43e693b3110caf4a6c57b91e4ca60acd9222
Nguyễn Ngọc Phương <1451010140Phuong@ou.edu.vn>
2017-11-13 10:42:01 +0700

    Update Project v3.8
    - Update Salary

--- EmployeeManagerment/EmployeeManagement_Service/Service/Module/Salary.cs
+++ EmployeeManagerment/EmployeeManagement_Service/Service/Module/Salary.cs
@@ -8,43 +8,43 @@ namespace EmployeeManagement_Service.Service.Module
 {
     public class Salary
     {
-        //private readonly EmployeeManagementDBContext _context;
+        private readonly EmployeeManagementDBContext _context;
 
-        //public Salary(EmployeeManagementDBContext context)
-        //{
-        //    this._context = context;
-        //}
+        public Salary(EmployeeManagementDBContext context)
+        {
+            this._context = context;
+        }
 
-        //public IEnumerable<P> EventsAll()
-        //{
-        //    return _context.PdbEvents;
-        //}
+        public IEnumerable<PdbBonusSalary> EventsAll()
+        {
+            return _context.PdbBonusSalaries;
+        }
 
-        //public bool Add(PdbEvent item)
-        //{
-        //    _context.PdbEvents.Add(item);
-        //    _context.Entry(item).State = System.Data.Entity.EntityState.Added;
-        //    return _context.SaveChanges() == 1;
-        //}
+        public bool Add(PdbBonusSalary item)
+        {
+            _context.PdbBonusSalaries.Add(item);
+            _context.Entry(item).State = System.Data.Entity.EntityState.Added;
+            return _context.SaveChanges() == 1;
+        }
 
-        //public bool Edit(PdbEvent item)
-        //{
-        //    _context.PdbEvents.Attach(item);
-        //    _context.Entry(item).State = System.Data.Entity.EntityState.Modified;
-        //    return _context.SaveChanges() == 1;
-        //}
+        public bool Edit(PdbBonusSalary item)
+        {
+            _context.PdbBonusSalaries.Attach(item);
+            _context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+            return _context.SaveChanges() == 1;
+        }
 
-        //public bool Delete(Guid id)
-        //{
-        //    PdbEvent item = _context.PdbEvents.SingleOrDefault(itemc => itemc.ID_Event == id);
-        //    _context.PdbEvents.DefaultIfEmpty(item);
-        //    _context.Entry(item).State = System.Data.Entity.EntityState.Deleted;
-        //    return _context.SaveChanges() == 1;
-        //}
+        public bool Delete(Guid id)
+        {
+            PdbBonusSalary item = _context.PdbBonusSalaries.SingleOrDefault(itemc => itemc.IDBS == id);
+            _context.PdbBonusSalaries.DefaultIfEmpty(item);
+            _context.Entry(item).State = System.Data.Entity.EntityState.Deleted;
+            return _context.SaveChanges() == 1;
+        }
 
-        //public PdbEvent GetEvent(Guid id)
-        //{
-        //    return _context.PdbEvents.SingleOrDefault(item => item.ID_Event == id);
-        //}
+        public PdbBonusSalary GetEvent(Guid id)
+        {
+            return _context.PdbBonusSalaries.SingleOrDefault(item => item.IDBS == id);
+        }
     }
 }
